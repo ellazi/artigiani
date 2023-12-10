970f4857ed0ca601aeda2ebb928e7070091bf5a7
Rossella Pellizzi <prellizzi@gmail.com>
2023-12-10 23:04:00 +0100

    seed events and styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require 'faker'
+require 'date'
 
 puts "Cleaning database..."
+Event.destroy_all
 User.destroy_all
 
 puts "Creating users..."
@@ -19,4 +21,26 @@ puts "Creating users..."
 end
 
 puts "Created #{User.count} users"
+
+puts "Creating events..."
+
+User.where(is_artisan: true).each do |user|
+  3.times do |i|
+    month = rand(1..12)
+    day = rand(1..28)
+    Event.create!(
+      title: Faker::Tea.variety,
+      description: Faker::Lorem.paragraph_by_chars,
+      location: Faker::Address.street_address + ", " + Faker::Address.city + ", " + Faker::Address.country,
+      start_time: DateTime.new(2024, month, day, 9, 0),
+      end_time: DateTime.new(2024, month, day, 18, 0),
+      user_id: user.id,
+      latitude: Faker::Address.latitude,
+      longitude: Faker::Address.longitude,
+    )
+  end
+end
+
+puts "Created #{Event.count} events"
+
 puts "Finished!"
